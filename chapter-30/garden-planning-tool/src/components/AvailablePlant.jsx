46cd1b6504ddcc0b7f071f4a4dc44b26f1710bf5
Carolista <cjones.renaissancewoman@gmail.com>
2024-05-06 09:38:30 -0400

    updated starter code for Ch 30

--- chapter-30/garden-planning-tool/src/components/AvailablePlant.jsx
+++ chapter-30/garden-planning-tool/src/components/AvailablePlant.jsx
@@ -1,14 +1,11 @@
-import { capitalize } from '../shared/utils';
+// TODO #1: Import capitalize function from utils
 
 const AvailablePlant = props => {
-  // TODO: Include image property and remove id (when no longer needed)
+
+  // TODO #1: Include image property and remove id (when no longer needed)
   let { id, name, color, numAvailable } = props.plant;
 
-  // TODO: Define two handlers for allocating and returning plants
-
-  // TODO: Replace simple text with a card containing image, text, and arrow icons as buttons
-
-  // TODO: Add click handlers to icons
+  // TODO #1: Replace simple text with a card containing image, text, and arrow icons as buttons
 
   return (
     <p id={id}>
--- chapter-30/garden-planning-tool/src/components/Main.jsx
+++ chapter-30/garden-planning-tool/src/components/Main.jsx
@@ -1,7 +1,8 @@
 import AvailablePlant from './AvailablePlant';
-// TODO: Import Flowerbed component
+// TODO #2: Import Flowerbed component
 
 const Main = () => {
+
   // Temporary test data
   let testPlants = [
     {
@@ -9,7 +10,7 @@ const Main = () => {
       name: 'Hyacinth',
       color: 'blue',
       image:
-        'https://drive.google.com/uc?export=view&id=1wZU8kWKcTWCIEMjz-Ce7dTU8Fcb-NM9I',
+        'https://drive.google.com/thumbnail?sz=w50&id=1wZU8kWKcTWCIEMjz-Ce7dTU8Fcb-NM9I',
       numAvailable: 6,
       numAllocated: 2,
     },
@@ -18,7 +19,7 @@ const Main = () => {
       name: 'Tulip',
       color: 'red',
       image:
-        'https://drive.google.com/uc?export=view&id=1Vl0Ch4MQslwh9EyhnGX1RK9YZbPuHtvo',
+        'https://drive.google.com/thumbnail?sz=w50&id=1Vl0Ch4MQslwh9EyhnGX1RK9YZbPuHtvo',
       numAvailable: 6,
       numAllocated: 6,
     },
@@ -27,31 +28,28 @@ const Main = () => {
       name: 'Daffodil',
       color: 'yellow',
       image:
-        'https://drive.google.com/uc?export=view&id=1BwQ_BdSgB6XGdU_OOm5iYQFXKjKEcW1Q',
+        'https://drive.google.com/thumbnail?sz=w50&id=1BwQ_BdSgB6XGdU_OOm5iYQFXKjKEcW1Q',
       numAvailable: 4,
       numAllocated: 12,
     },
   ];
 
-  // TODO: Use .map() to iterate over plants array
+  // TODO #3: Use .map() to iterate over plants array
   // and create multiple instances of the AvailablePlant component
   // Use plant id as key
-  let availablePlantsJSX = testPlants.map(plant => {
-    return <AvailablePlant key={plant.id} plant={plant} />;
-  });
 
   return (
     <main>
       <div id="left-column">
         <h3>Available Plants</h3>
-        {/* TODO: replace these 3 with mapped JSX */}
+        {/* TODO #3: replace these 3 with mapped JSX */}
         <AvailablePlant plant={testPlants[0]} />
         <AvailablePlant plant={testPlants[1]} />
         <AvailablePlant plant={testPlants[2]} />
       </div>
 
       <div id="right-column">
-        {/* TODO: Create and use Flowerbed component */}
+        {/* TODO #2: Create and use Flowerbed component */}
       </div>
     </main>
   );
